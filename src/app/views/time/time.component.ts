3fe6a8b4a9fd539cb3ac412e513d61cf80769840
RodrigoMoreiraSilva <rodrigo.tianalista@gmail.com>
2021-09-29 14:31:45 -0300

    Binds de Eventos e atributos

--- src/app/views/time/time.component.ts
+++ src/app/views/time/time.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeComponent implements OnInit {
 
+  propId = 'Antes'
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  bindDeEvento(): void {
+    console.log('Fazendo algo!')
+
+    this.propId = 'Depois'
+  }
 }
