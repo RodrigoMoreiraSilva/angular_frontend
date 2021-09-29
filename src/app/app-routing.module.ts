c325761f4fdab6930ee0d79e799677a82b23b241
RodrigoMoreiraSilva <rodrigo.tianalista@gmail.com>
2021-09-29 10:46:06 -0300

    Primeira rota (time)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {HomeComponent} from "./views/home/home.component";
 import {ProductCrudComponent} from "./views/product-crud/product-crud.component";
+import {TimeComponent} from "./views/time/time.component";
 
 const routes: Routes = [{
   path:"",
@@ -11,6 +12,10 @@ const routes: Routes = [{
 {
   path:"products",
   component: ProductCrudComponent
+},
+{
+  path:"time",
+  component: TimeComponent
 }
 ];
 
